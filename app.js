e4a8c407da8d76339374d3b9c164c74ec7e0974d
Christopher Dazzo <dazzo.chris@gmail.com>
2022-05-03 17:45:33 -0500

    styles

---
+++ app.js
@@ -0,0 +1,11 @@
+// section 1
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+// section 2
+const section = document.querySelector('.section2');
+const end = section.querySelector('h1');
+
+
+
