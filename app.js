757ee0ff9210967e18b65dc21a99157b3b343dc3
Christopher Dazzo <dazzo.chris@gmail.com>
2022-05-03 21:53:56 -0500

    video ffmpeg

--- app.js
+++ app.js
@@ -7,31 +7,31 @@ const text = intro.querySelector('h1');
 const section = document.querySelector('.section2');
 const end = section.querySelector('h1');
 
-// ScrollMagic
-const controller = new ScrollMagic.Controller()
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
 
-// Scenes
-const scene = new ScrollMagic.Scene({
-    duration: 7000,
-    triggerElement: intro,
-    triggerHook: 0
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0
 })
-.addIndicators()
-.setPin(intro)
-.addTo(controller);
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
 
-// Video Animation
+//Video Animation
 let accelamount = 0.1;
 let scrollpos = 0;
 let delay = 0;
 
-scene.on('update', e => {
-    scrollpos = e.scrollPos / 1000;
-    // console.log(e);
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
 });
 
 setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-    // console.log(scrollpos);
-    video.currentTime = scrollpos;
-}, 41.6);
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3);
